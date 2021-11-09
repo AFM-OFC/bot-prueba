e8ce26fceb95c4fa4cb290e6b0a116c529ec98f6
AFM-OFC <90519341+AFM-OFC@users.noreply.github.com>
2021-11-09 13:11:49 +0100

    Update deep.js

--- deep.js
+++ deep.js
@@ -2,23 +2,23 @@ const { WAConnection } = require('@adiwajshing/baileys');
 const fs = require('fs');
 
 async function iniciar () { 
-        const client = new WAConnection()
+        const deusafm = new WAConnection()
 
-        client.logger.level = 'warn'
+        deep.logger.level = 'warn'
 
-        client.on('qr', () => {
+        deusafm.on('qr', () => {
         })
 
         fs.existsSync('./conect/deep.json') && client.loadAuthInfo('./conect/deep.json')
 
-        client.on('connecting', () => {
+        deusafm.on('connecting', () => {
         console.log('Conectando DeepBot')
         })
 
-        client.on('open', () => {
+        deusafm.on('open', () => {
         console.log('DeepBot Conectado exitosamente :D')
         })
-        await client.connect({timeoutMs: 30*1000})
+        await deusafm.connect({timeoutMs: 30*1000})
         fs.writeFileSync('./conect/deep.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
         }
 
